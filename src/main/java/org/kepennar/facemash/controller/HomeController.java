2b7ba3036cda8634204eb9046a29496e3bce144a
KePennar <kevin.pennarun@gmail.com>
2012-10-25 23:39:11 +0200

    Renaming home method

--- src/main/java/org/kepennar/facemash/controller/HomeController.java
+++ src/main/java/org/kepennar/facemash/controller/HomeController.java
@@ -1,7 +1,5 @@
 package org.kepennar.facemash.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,14 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/")
 public class HomeController {
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String homeForm() {
+	public String templateIndex() {
 		return "index";
 	}
 
