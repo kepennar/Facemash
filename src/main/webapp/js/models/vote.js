379276c25e5414a3fca38e8d15059c2aa1477392
KePennar <kevin.pennarun@gmail.com>
2012-11-07 12:58:31 +0100

    Change model url

--- src/main/webapp/js/models/vote.js
+++ src/main/webapp/js/models/vote.js
@@ -2,7 +2,7 @@ define([ 'underscore', 'backbone' ], function(_, Backbone) {
 	var VoteModel = Backbone.Model.extend({
 
 		url : function() {
-			return '/api/vote';
+			return 'api/vote';
 		},
 		// Remove this Sample from *localStorage*.
 		clear : function() {
